fff8fee2bc364c8a21a2cca1854cf36c0b182883
bgreni <42788181+bgreni@users.noreply.github.com>
2023-01-20 03:11:58 +0000

    testing

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+// import * as anchor from "@project-serum/anchor";
+// import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
+// import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
+
 
 (async () => {
-    const pk = process.env.KEY;
+    const pk = core.getInput('wallet-key');
     const payload = github.context.payload;
+    console.log(payload);
 
-    console.log(JSON.stringify(payload), undefined, 2);
-    console.log(`PK: ${pk}`);
+    // const programId = "FAuRwCnsvpMHVBDcL47SGM5XSC7oY5u5u9VU3GDqWaZm";
+    // let connection = new Connection(clusterApiUrl("devnet"));
+    // const wallet = new NodeWallet(Keypair.fromSecretKey(TexEncoder().encode(pk)));
+    // const provider = new anchor.AnchorProvider(connection, wallet, anchor.AnchorProvider.defaultOptions());
+
+    // const program = await new anchor.Program.at(programId, provider);
+
+    // await program.methods.
+    //     .releaseBounty()
+    //     .accounts({
+    //         bounty: null,
+    //         poster: wallet.publickKey,
+    //     }).rpc()
 })();
